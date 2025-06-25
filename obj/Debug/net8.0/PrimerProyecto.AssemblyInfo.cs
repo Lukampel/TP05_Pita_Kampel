94d44a90df2084de1482a4ea3be404f24490f504
RAMIRO PITA <49261291@ort>
2025-06-25 08:01:33 -0300

    .

--- obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac492930909bfc1e42bd03aaa84c534b4b900b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1291e343fba13a8f12948218a79eadf21dbae67")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
